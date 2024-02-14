ac5dfc56c6607a90abdbae0dd10b891fcf3aec3d
Ann Connolly <biacatering4u@gmail.com>
2024-02-13 16:02:15 -0800

    updates

--- csv_import.py
+++ csv_import.py
@@ -24,32 +24,27 @@ class CSVWriter:
        with open(fileName, 'w', newline='') as file:
         csv_writer = csv.writer(file)
         for row in data:
-            csv_writer.writerow(row)
-    def pop(self,list):
-        for item in list:
-            if item[1] < list[0][1]:
-                item.pop()
-        return list
+            csv_writer.writerow(row) 
 
 writer=CSVWriter()
 
-for key,group in grouped_list:
+for key,group in grouped_list:  
     for item in group:
         sumOfQuant.append(int(item[2]))
         
     average = int(sum(sumOfQuant) / int(len(sumOfQuant)))
     data1Output.append([key,average])
-    writer.write(csv_file_out_path1,data1Output)
-    
     data2Output.append([key,int(len(sumOfQuant))])
-    data2Sorted = sorted(data2Output,key=lambda x:x[1],reverse=True)
-    topN = max(data2Sorted)[1]
-    topN_filtered = list(filter(lambda x: x[1] > topN, data2Sorted))
-
-    writer.write(csv_file_out_path2,topN_filtered)
-  
     sumOfQuant =[]
-    print()
+    
+writer.write(csv_file_out_path1,data1Output)
+data2Sorted = sorted(data2Output,key=lambda x:x[1],reverse=True)
+topN= data2Sorted[0][1]
+topN_filtered = list(filter(lambda x: x[1] == topN, data2Sorted))
+
+writer.write(csv_file_out_path2,topN_filtered)
+
+print()
 
       
 
